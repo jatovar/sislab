0260d865a91b902641a450365275157650d5e232
AdrianCerda <adriancerda17@gmail.com>
2015-11-19 13:57:42 -0600

    Se agrego el archivo ProfesorController

---
+++ app/Http/Controllers/ProfesorController.php
@@ -0,0 +1,31 @@
+<?php namespace App\Http\Controllers;
+use App\Models\Profesor;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent;
+
+/**
+ *
+ */
+class ProfesorController extends Controller
+{
+
+  function dameNombre(Request $r)
+  {
+    $clave = $r->input('clave_profesor');
+    $res = ['success' => false];
+    try {
+      $profesor = Profesor::find($clave);
+      $res['nombre'] = "";
+      if($profesor!=null)
+      {
+        $res['nombre'] = $profesor->nombreCompleto();
+        $res ['success'] = true;
+      }
+
+    } catch (Exception $e) {
+      $res['msj'] = $e->getMessage();
+    }
+    return response()->json($res);
+  }
+}
